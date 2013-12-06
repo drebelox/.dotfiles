bab96b291ded9a0ab2f0c1d4cd9c531d3481d4b1
euforic <cs@euforic.co>
2013-12-05 18:13:34 -0800

    fix path

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ hash git >/dev/null && /usr/bin/env git clone https://github.com/euforic/.dotfil
 }
 
 echo -e "\033[0;34mRunning Setup\033[0m"
-./~/.dotfiles/setup.sh
+sh ~/.dotfiles/setup.sh
 
 echo -e "\033[0;34mLinking Dotfiles\033[0m"
-./~/.dotfiles/link.sh
+sh ~/.dotfiles/link.sh
