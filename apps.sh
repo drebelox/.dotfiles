404ca26121816398d63a85cfd9f3fa00286ae8ae
Christian Sullivan <cs@euforic.co>
2014-02-03 12:33:09 -0800

    remove oh-my-zsh and only link custom plugin, link sublime text 3 packages instead of copy and add base apps install via homebrew casks

---
+++ apps.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# browser
+brew cask install google-chrome
+brew cask install google-chrome-canary
+
+# development
+brew cask install sublime-text-3
+brew cask install iterm2
+brew cask install mou
+brew cask install genymotion
+
+# virtual machines
+brew cask install virtualbox
+brew cask install vagrant
+
+# file storage
+brew cask install dropbox
+brew cask install google-drive
+
+# chat
+brew cask install linkinus
+brew cask install flowdock
+brew cask install google-hangouts
+
+# media
+brew cask instlal vlc
+
+# utils
+brew cask install alfred
+brew cask install knock
+brew cask install caffeine
+brew cask install u-torrent
